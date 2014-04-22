12e20de7e7a8056a890e5388bcc0f97b4181e4aa
Russell Osborne <russosborn@gmail.com>
2014-04-22 11:20:34 -0400

    Creating More Folders via chef (release/shared)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'russell@burningpony.com'
 license 'Apache 2.0'
 description 'Installs Timecard Timekeeping Application on ubuntu boxes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.5'
+version '0.0.6'
 
 depends 'apt'
 
--- recipes/debian.rb
+++ recipes/debian.rb
@@ -2,7 +2,23 @@
 directory node['install_location'] do
   owner 'deploy'
   group 'deploy'
-  mode 00744
+  mode 2755
   action :create
-  recursive True
+  recursive true
+end
+
+directory node['install_location'] + '/releases' do
+  owner 'deploy'
+  group 'deploy'
+  mode 2755
+  action :create
+  recursive true
+end
+
+directory node['install_location'] + '/shared' do
+  owner 'deploy'
+  group 'deploy'
+  mode 2755
+  action :create
+  recursive true
 end
