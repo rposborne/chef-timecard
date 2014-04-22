015939005cd0c68eedb821a396f4d287994e98e5
Russell Osborne <russosborn@gmail.com>
2014-04-22 10:30:54 -0400

    Fixed Default attr.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 # check http://corefonts.sourceforge.net/ for later versions
 # current version as of 2012-12
-default['timecard']['install_location'] = '/var/www/timecard'
+default['install_location'] = '/var/www/timecard'
